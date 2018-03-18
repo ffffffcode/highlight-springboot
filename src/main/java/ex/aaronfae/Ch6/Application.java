2f6c0c539ddf349d8e82550a183304ed69430267
AaronFae <AaronFae@163.com>
2018-03-18 16:43:49 +0800

    6.2.3 类型安全的配置

--- src/main/java/ex/aaronfae/Ch6/Application.java
+++ src/main/java/ex/aaronfae/Ch6/Application.java
@@ -1,5 +1,6 @@
 package ex.aaronfae.Ch6;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,9 @@ public class Application {
     @Value("${project.author}")
     private String author;
 
+    @Autowired
+    private AuthorProperties authorProperties;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
@@ -20,5 +24,10 @@ public class Application {
     @RequestMapping("/")
     String index() {
         return "Hello Spring Boot - " + author;
+    }
+
+    @RequestMapping("/author")
+    String author() {
+        return "author name is " + authorProperties.getName() + " author age is " + authorProperties.getAge();
     }
 }
