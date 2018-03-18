7591e351aeb1ba2830186b04dc363e72242c037d
AaronFae <AaronFae@163.com>
2018-03-18 16:27:30 +0800

    6.2.2 常规属性配置

---
+++ src/main/java/ex/aaronfae/Ch6/Application.java
@@ -0,0 +1,24 @@
+package ex.aaronfae.Ch6;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Application {
+
+    @Value("${project.author}")
+    private String author;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping("/")
+    String index() {
+        return "Hello Spring Boot - " + author;
+    }
+}
